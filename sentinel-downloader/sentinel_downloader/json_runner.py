6d6be936f4820794918e9ae7a244c88ba48f9477
PedroPertusi <pedrovazpertusi@gmail.com>
2024-10-10 14:54:16 -0300

    feat: adding json_runner to run package with a config.json

--- sentinel-downloader/sentinel_downloader/json_runner.py
+++ sentinel-downloader/sentinel_downloader/json_runner.py
@@ -1,4 +1,3 @@
-from argparse import ArgumentParser
 from sentinel1 import Sentinel1
 from sentinel2 import Sentinel2
 from utils import divide_big_area, create_dir, load_evalscript
@@ -7,24 +6,28 @@ from image_processing import process_image, normalize, png_conversion
 import ast
 from datetime import datetime
 import shutil
+import json
 
 def main():
-    parser = ArgumentParser(description="Sentinel-Downloader API")
-    # Choose between sentinel 1 and sentinel 2
-    parser.add_argument("-s", "--satellite", type=str, required=True)
-
-    # All satellites
-    parser.add_argument("-c", "--coords", type=str, required=True)
-    parser.add_argument("-t", "--time-interval", type=str, required=True)
-    parser.add_argument("-r", "--resolution", type=int, required=False, default=512)
-    parser.add_argument("-sd", "--save-dir", type=str, required=False, default=f"{datetime.now().strftime('%Y-%m-%d-%H:%M:%S')}")
-    parser.add_argument("-f", "--filename", type=str, required=False, default="file")
-
-    # Only for sentinel 2
-    parser.add_argument("-ev", "--evalscript", type=str, required=False, default="rgb")
-    parser.add_argument("-cr", "--cloud-removal", type=bool, required=False, default=False)
-
-    args = parser.parse_args()
+    with open("../config.json") as f:
+        data = json.load(f)
+    
+    
+    if "satellite" not in data:
+        raise ValueError("No satellite specified, please input a satellite (sentinel1, sentinel2, or both).")
+    if "coords" not in data:
+        raise ValueError("No coordinates specified, please input coordinates.")
+    if "time_interval" not in data:
+        raise ValueError("No time interval specified, please input a time interval.")
+    
+    satellite = data["satellite"]
+    coords = data["coords"]
+    time_interval = data["time_interval"]
+    resolution = data["resolution"] if "resolution" in data else 512
+    save_dir = data["save_dir"] if "save_dir" in data else f"{datetime.now().strftime('%Y-%m-%d-%H:%M:%S')}"
+    filename = data["filename"] if "filename" in data else "file"
+    evalscript = data["evalscript"] if "evalscript" in data else "rgb"
+    cloud_removal = data["cloud_removal"] if "cloud_removal" in data else False
 
     save_dir_created = False
 
@@ -32,37 +35,34 @@ def main():
         # Error handling
 
         # Check if satellite is valid
-        satellite = satellite_error_handling(args.satellite)
+        satellite = satellite_error_handling(satellite)
 
-        coords = ast.literal_eval(args.coords)
+        coords = ast.literal_eval(coords)
         coordinate_error_handling(coords)
         coords = (coords[1], coords[0], coords[3], coords[2])
         
 
-        time_interval = ast.literal_eval(args.time_interval)
+        time_interval = ast.literal_eval(time_interval)
         time_interval_error_handling(time_interval)
 
 
-        resolution_error_handling(args.resolution, satellite)
-        resolution = (args.resolution, args.resolution)
-        step = 0.0459937425 * args.resolution / 512
+        resolution_error_handling(resolution, satellite)
+        resolution = (resolution, resolution)
+        step = 0.0459937425 * resolution[0] / 512
 
-        save_dir_error_handling(args.save_dir)
+        save_dir_error_handling(save_dir)
         # save_dir = os.path.join(os.getcwd(), "output", args.save_dir) --> change to users cwd?
-        save_dir = f"./output/{args.save_dir}"
+        save_dir = f"./output/{save_dir}"
         create_dir(save_dir, satellite)
         save_dir_created = True
 
-        filename_error_handling(args.filename)
-        filename = args.filename
+        filename_error_handling(filename)
 
         if satellite == "sentinel2" or satellite == "both":
             
-            evalscript = args.evalscript
             evalscript_error_handling(evalscript)
             evalscript = load_evalscript(evalscript)
 
-            cloud_removal = args.cloud_removal
             cloud_removal_error_handling(cloud_removal)
 
             sentinel2 = Sentinel2()
@@ -91,7 +91,6 @@ def main():
             vv_vh_list, filenames = process_image(save_dir)
 
             image_final_list = normalize(vv_vh_list)
-            print(resolution)
             png_conversion(image_final_list, filenames, save_dir, resolution[0])
             
     except Exception as e:
