4577474981d7d7503f4c7e245e548f7f14901877
alemagno10 <alemagno03@gmail.com>
2024-10-14 19:47:23 +0000

    FEAT: creating scripts for exporting models and a template for .env necessary for model training

---
+++ flood-segmentation/commands/export.sh
@@ -0,0 +1,6 @@
+python ../../config/create_mount.py
+tao model unet export -m $TAO_EXPERIMENT_DIR/resnet18/USA/weights/model.tlt \
+                 -e $TAO_SPECS_DIR/resnet18/combined_config.txt \
+                 -k $NVIDIA_API_TOKEN \
+                 --engine_file $TAO_EXPERIMENT_DIR/export/sample_resnet18.engine \
+                 --gen_ds_config
