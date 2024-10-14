729003f6f284c75ad0706f7f0f91ec612572d60e
Joao Lucas <joaolucasmbcadorniga@gmail.com>
2024-10-14 16:19:27 -0300

    FEAT: adding shell script to download most important NVIDIA features used for training and using models

---
+++ flood-segmentation/README.md
@@ -0,0 +1,9 @@
+* You must have [Docker](https://docs.docker.com/engine/install/) installed
+
+* To download the data used for training, follow the instructions [here](https://github.com/cloudtostreet/Sen1Floods11/tree/master)
+
+* To download our pre-trained model, click [here](https://drive.google.com/drive/folders/1O2mb3NHJiUIS35v9NionneKBnZAg4C5G?usp=sharing)
+
+Steps: 
+
+#TODO
---
+++ flood-segmentation/commands/setup.sh
@@ -0,0 +1,47 @@
+# Install requirements
+pip install -r ../requirements.txt
+
+# Load the .env file
+source ../config/.env
+
+# Create mount
+python ../config/create_mount.py
+
+# User must have been Docker login
+
+# Creating nvcr.io config
+apikey=$NVIDIA_API_KEY
+echo ";WARNING - This is a machine generated file.  Do not edit manually." > config
+echo ";WARNING - To update local config settings, see \"ngc config set -h\"" >> config
+echo "" >> config
+echo "[CURRENT]" >> config
+echo "apikey=${apikey}" >> config
+echo "format_type=json" >> config
+echo "org=nvidia" >> config
+
+rm -rf ~/.ngc
+mkdir -p ~/.ngc & mv config ~/.ngc/
+
+# Login into NVIDIA ngc
+docker login -u '$oauthtoken' -p $apikey nvcr.io
+
+# Download NVIDIA container toolkit
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+
+sudo apt-get install -y nvidia-container-toolkit
+
+# THIS ASSUMES YOU ALREADY HAVE INSTALLED THE PROPER NVIDIA DRIVERS FOR YOUR GPU
+
+# Creating folders
+mkdir -p $LOCAL_PROJECT_DIR
+mkdir -p $LOCAL_DATA_DIR/v$VERSION
+mkdir -p $LOCAL_SPECS_DIR/v$VERSION
+mkdir -p $LOCAL_PROJECT_DIR/v$VERSION
+
+# Pulling the container image and testing
+tao model unet --help
